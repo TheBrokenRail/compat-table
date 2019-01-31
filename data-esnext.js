b715bb5b79942e10754d298d85017fd62e6b6426
Denis Pushkarev <zloirock@zloirock.ru>
2019-01-31 07:12:26 +0700

    move richer keys to stage 2

--- data-esnext.js
+++ data-esnext.js
@@ -2880,7 +2880,7 @@ exports.tests = [
 },
 {
   name: 'richer keys',
-  category: STAGE1,
+  category: STAGE2,
   significance: 'small',
   spec: 'https://github.com/bmeck/proposal-richer-keys/',
   subtests: [
